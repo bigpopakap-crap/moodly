eef57294ee550f5b1efadf9950af2ec75e6e0fcf
bigpopakap <bigpopakap@gmail.com>
2019-02-16 22:55:44 -0800

    Add eslint, prettier, stylelint, use recommended rules, and accessibility rules

--- client/src/components/MyInput.tsx
+++ client/src/components/MyInput.tsx
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 import styles from './MyInput.module.scss';
 
 export default class extends Component {
-    render() {
-        return (
-            <input type="text" className={styles.input}/>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <input type="text" className={styles.input} />
+      </div>
+    );
+  }
 }
--- client/src/components/MyTextArea.tsx
+++ client/src/components/MyTextArea.tsx
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 import styles from './MyTextArea.module.scss';
 
 export default class extends Component {
-    render() {
-        return (
-            <textarea className={styles.input}/>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <textarea className={styles.input} />
+      </div>
+    );
+  }
 }
