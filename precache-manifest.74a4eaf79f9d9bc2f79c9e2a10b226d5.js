5d2d523d6436287576ffd2960d1aefcd8e43ce1e
bigpopakap <bigpopakap@gmail.com>
2019-02-16 20:55:07 -0800

    Updates

---
+++ precache-manifest.74a4eaf79f9d9bc2f79c9e2a10b226d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "97bada1e85d0c15c28a7",
+    "url": "/moodly/static/css/main.44985e7a.chunk.css"
+  },
+  {
+    "revision": "97bada1e85d0c15c28a7",
+    "url": "/moodly/static/js/main.97bada1e.chunk.js"
+  },
+  {
+    "revision": "b6ecadbfd31132a31b33",
+    "url": "/moodly/static/js/runtime~main.b6ecadbf.js"
+  },
+  {
+    "revision": "063ce5bc9f8a30d66648",
+    "url": "/moodly/static/js/2.063ce5bc.chunk.js"
+  },
+  {
+    "revision": "72cda7cc870459e8182685c0f48075f7",
+    "url": "/moodly/index.html"
+  }
+];
