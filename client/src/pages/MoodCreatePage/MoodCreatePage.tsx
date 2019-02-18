695c56d5e848346f8f2c07c71cf6d2d3d1b0bd72
bigpopakap <bigpopakap@gmail.com>
2019-02-18 15:41:15 -0800

    Start making a form input to submit data

--- client/src/pages/MoodCreatePage/MoodCreatePage.tsx
+++ client/src/pages/MoodCreatePage/MoodCreatePage.tsx
@@ -1,4 +1,6 @@
 import React, { Component, ReactNode } from 'react';
+import FormControl from '@material-ui/core/FormControl';
+import { FormHelperText, InputLabel, TextField } from '@material-ui/core';
 import Layout from '../Layout/Layout';
 import styles from './MoodCreatePage.module.scss';
 
@@ -6,7 +8,13 @@ export default class MoodCreatePage extends Component {
   public render(): ReactNode {
     return (
       <Layout>
-        <div className={styles.MoodCreatePage}>Mood create page!</div>
+        <div className={styles.MoodCreatePage}>
+          <FormControl>
+            <InputLabel htmlFor="descriptionInput">Description</InputLabel>
+            <TextField id="descriptionInput" aria-describedby="descriptionHelperText" multiline />
+            <FormHelperText id="descriptionHelperText">This is the description.</FormHelperText>
+          </FormControl>
+        </div>
       </Layout>
     );
   }
