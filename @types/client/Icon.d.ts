f957fa6616a9fdb1545b7cb72c1b3c3200a07169
bigpopakap <bigpopakap@gmail.com>
2019-02-20 21:19:16 -0800

    Take first pass at defining the data model

---
+++ @types/client/Icon.d.ts
@@ -0,0 +1,11 @@
+/**
+ * An enum of all the keys of icons that the UI knows how to render.
+ */
+export enum IconKey {
+    BRIEFCASE = 'briefcase',
+    HEART = 'heart',
+    EXERCISE = 'excercise',
+    FAMILY = 'family',
+    MALE_PERSON = 'male_person',
+    STETHOSCOPE = 'stethoscope'
+}
